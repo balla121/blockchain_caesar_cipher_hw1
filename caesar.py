19af1b26549f8c125eca0561da11318cebbbc1b4
Weijie Gao <gweijie01@gmail.com>
2021-05-13 17:41:17 -0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -2,13 +2,13 @@
 def encrypt(key,plaintext):
     ciphertext=""
     
-#     for i in plaintext:
-#       if i.isalpha():
-#         alphabet = ord(i)+key
-#         if alphabet > ord("Z"):
-#           alphabet -= 26
-#         letter = chr(alphabet)
-#         ciphertext+=letter
+    for i in plaintext:
+      if i.isalpha():
+        alphabet = ord(i)+key
+        if alphabet > ord("Z"):
+          alphabet -= 26
+        letter = chr(alphabet)
+        ciphertext+=letter
     
     return ciphertext
 
