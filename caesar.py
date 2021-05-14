a33f17af9e6d9db18c83e293721316b773073474
Weijie Gao <gweijie01@gmail.com>
2021-05-13 17:40:59 -0700

    Create caesar.py

---
+++ caesar.py
@@ -0,0 +1,20 @@
+
+def encrypt(key,plaintext):
+    ciphertext=""
+    
+#     for i in plaintext:
+#       if i.isalpha():
+#         alphabet = ord(i)+key
+#         if alphabet > ord("Z"):
+#           alphabet -= 26
+#         letter = chr(alphabet)
+#         ciphertext+=letter
+    
+    return ciphertext
+
+def decrypt(key,ciphertext):
+    plaintext=""
+    
+    return plaintext
+
+
